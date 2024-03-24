123d50ecfeaff6a111f5ebc2d784f295ccb6dd50
Bhavesh Charde <115037516+BhaveshC07@users.noreply.github.com>
2024-03-24 23:53:36 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Reverse a Stack/reverse-a-stack.java
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+        PrintWriter out=new PrintWriter(System.out);
+        
+        //taking testcases
+        int t = Integer.parseInt(br.readLine()); 
+    	for(int i=0;i<t;i++)
+    	{
+            String str=br.readLine();
+    		
+    		//input n and d
+    	    int n=Integer.parseInt(str);
+    		Stack<Integer> stack=new Stack<>();
+    		String str1=br.readLine();
+    		String[] starr1=str1.split(" ");
+    		//inserting elements in the array
+    		for(int j=0;j<n;j++)
+    		{
+    		    stack.push(Integer.parseInt(starr1[j]));
+    		}
+    		//calling reverse() function
+            Solution.reverse(stack);
+            for(int j:stack){
+                out.print(j+" ");
+            }
+            out.println();
+         }
+         out.close();
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{ 
+    static void reverse(Stack<Integer> s)
+    {
+        // add your code here
+        Stack<Integer> s1=new Stack<>();
+        Stack<Integer> s2=new Stack<>();
+        while(!s.isEmpty()){
+            s1.push(s.pop());
+        }
+        while(!s1.isEmpty()){
+            s2.push(s1.pop());
+        }
+        while(!s2.isEmpty()){
+            s.push(s2.pop());
+        }
+    }
+    
+}
