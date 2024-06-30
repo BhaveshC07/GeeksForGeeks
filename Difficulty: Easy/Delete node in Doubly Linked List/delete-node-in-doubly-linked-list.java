8c6b52d1293302f660e4f90c384f6115dd9bca82
Bhavesh Charde <115037516+BhaveshC07@users.noreply.github.com>
2024-06-30 23:39:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Delete node in Doubly Linked List/delete-node-in-doubly-linked-list.java
@@ -0,0 +1,90 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+    Node prev;
+
+    Node(int x) {
+        data = x;
+        next = null;
+        prev = null;
+    }
+
+    public static void printList(Node node) {
+        while (node != null) {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            Node head = null;
+            Node tail = head;
+
+            for (int i = 0; i < n; i++) {
+                int temp = sc.nextInt();
+                if (head == null) {
+                    head = new Node(temp);
+                    tail = head;
+                } else {
+                    Node newNode = new Node(temp);
+                    tail.next = newNode;
+                    newNode.prev = tail;
+                    tail = newNode;
+                }
+            }
+            int x = sc.nextInt();
+
+            Solution obj = new Solution();
+            Node res = obj.deleteNode(head, x);
+
+            Node.printList(res);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+/*
+
+Definition for doubly Link List Node
+class Node
+{
+    int data;
+    Node next;
+    Node prev;
+    Node(int x){
+        data = x;
+        next = null;
+        prev = null;
+    }
+}
+*/
+class Solution {
+    public Node deleteNode(Node head, int x) {
+        if(x==1){
+            head.next.prev=null;
+            return head.next;
+        }
+        Node curr=head;
+        for(int i=1;i<x;i++){
+            curr=curr.next;
+        }
+        curr.prev.next=curr.next;
+        if(curr.next!=null){
+            curr.next.prev=curr.prev;
+        } 
+        return head;
+    }
+}
